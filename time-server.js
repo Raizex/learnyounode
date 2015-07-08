06bab9cf7dfc4e97b5f6ec3eea0419c1803e8cb7
Justin Huffman <jwhuffman@gmail.com>
2015-07-08 15:45:00 -0600

    Completed time server challenge.

--- time-server.js
+++ time-server.js
@@ -1,11 +1,13 @@
 var net = require('net');
+var strftime= require('strftime')
 var port = process.argv[2];
 var timeServer = net.createServer(function (socket) {
     console.log('Client connected from ' + socket.remoteAddress);
     socket.on('end', function() {
         console.log('Client disconnnected');
     });
-    socket.write('hello\r\n');
+    socket.write(strftime('%Y-%m-%d %H:%M\r\n'));
+    socket.end();
 });
 timeServer.listen(port, function() {
     console.log('Server bound on port: ' + port);
